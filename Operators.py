19dda8ef298fc974f1ac36c9bc7dcfc8313c112c
jaya-lakshmi-11222 <jayachowdary11222@gmail.com>
2025-08-01 12:59:19 +0530

    variables

---
+++ Operators.py
@@ -0,0 +1,36 @@
+#Arithmetic operators
+x=int(input())
+y=int(input())
+p = x+y
+q = x-y
+r = x*y
+s = x//y
+t = x%y
+print(p, q, r, s, t)
+
+#break
+
+#Logical Operators
+a=int(input())
+b=int(input())
+p = a and b
+q = a or b
+r = not a
+print(p,q,r)
+
+#break
+
+#Bitwise operators
+a=int(input())
+b=int(input())
+c=int(input())
+d=a^a
+e=c^b
+f=a&b
+g=c | (a^a)
+e= ~e
+print(d, e, f, g, sep=' ')
+
+#break
+
+
---
+++ Variables.py
@@ -0,0 +1,35 @@
+#Type conversion float to int
+d = float(input())
+print(int(d))
+
+#break
+
+#Typecast and double it
+num = input()
+print(int(num)*2)
+
+#break
+
+#swap the numbers
+a = int(input())
+b = int(input())
+a,b=b,a
+print(a, b)
+
+#break
+
+#Sum of n natural numbers
+n = int(input())
+total=0
+for i in range(1,n+1):
+  print(i)
+  total += i
+print(total)
+
+#Sum of n natural numbers without using loops
+n=int(input())
+sum = n*(n+1)//2
+print(sum)
+
+#break
+
